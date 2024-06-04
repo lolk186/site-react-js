bf8eb726185d93c30dfb2751e8701ad674e0baca
cybersport <bzuev662@gmail.com>
2024-06-04 16:01:10 +0300

    4 july

--- bicycle/src/App.js
+++ bicycle/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import BodyComponent from './components/BodyComponent';
 import "../src/components/css/html.css"
 import AboutComponent from "./components/AboutComponent"
+import ServiceComponent from './components/ServiceComponent';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <HeaderComponent/>
         <BodyComponent/>
         <AboutComponent/>
+        <ServiceComponent/>
     </div>
   );
 }
--- bicycle/src/components/ServiceComponent.jsx
+++ bicycle/src/components/ServiceComponent.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import "../components/css/ServiceComponent.css"
+import img_back from "../images/aff8de14165da69c035537a9bbc2088a.png"
 
 function ServiceComponent() {
   return (
-    <div>ServiceComponent</div>
+    <div class="dive_wbike">
+        <img src={img_back} class="img_bike1"></img>
+        <b class="text_3">Service,<br/>
+            maintenance and<br/>
+            insurance</b>
+        <p class="text_4">The lease includes an all-in service package.<br/>
+            Throughout the entire 36 months, you are<br/>
+            fully insured against damage, theft and<br/>
+            maintenance. In addition, you always have<br/>
+            access to roadside assistance. </p>
+
+    </div>
   )
 }
 
